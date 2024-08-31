804c800725b666e484a2be4b0044cc377d85fe16
jaeyoung99-lee <leejy0715@naver.com>
2024-08-31 22:34:11 +0900

    Navbar style

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -17,10 +17,13 @@ const AppLayout = () => {
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
               alt="Netflix Logo"
-              style={{ width: "100px" }}
+              style={{ width: "100px", height: "auto" }}
             />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Toggle
+            aria-controls="navbarScroll"
+            style={{ borderColor: "red" }}
+          />
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0"
