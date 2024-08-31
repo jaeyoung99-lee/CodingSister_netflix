fd96ad2faf60e9554752d538040e9fdccfd55bf4
jaeyoung99-lee <leejy0715@naver.com>
2024-08-31 20:43:22 +0900

    Route 구성 : nested route(라우트 안에 라우트 : 중첩된 라우트)와 Outlet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import AppLayout from "./layout/AppLayout";
+import Homepage from "./pages/Homepage/Homepage";
+import MoviePage from "./pages/Movies/MoviePage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import NotFoundPage from "./pages/NotFoundpage/NotFoundPage";
+
+// 홈페이지 -> /
+// 영화 전체 보여주는 페이지(서치 기능 포함) -> /movies
+// 영화 디테일 페이지 -> /movies/:id
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={AppLayout}>
+        {/* 부모의 path와 같을 경우 index 써도 됨 */}
+        <Route index element={<Homepage />} />
+        {/* path의 일부분이 같을 경우 Route로 한 번 더 감싸줘도 됨 */}
+        {/* <Route path="/movies" element={<MoviePage />} />
+        <Route path="/movies/:id" element={<MovieDetailPage />} /> */}
+        <Route path="movies">
+          <Route index element={<MoviePage />} />
+          <Route path=":id" element={<MovieDetailPage />} />
+        </Route>
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
